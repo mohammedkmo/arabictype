15b6c7d405e15a73b2233f25d6bd856cf1be9149
mohammed kareem <alrasamking123@gmail.com>
2020-09-07 19:43:27 +0300

    first upload

---
+++ code.js
@@ -0,0 +1,40 @@
+// This plugin will open a modal to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (see documentation).
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.resize(300, 400);
+figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
+    // One way of distinguishing between different types of messages sent from
+    // your HTML page is to use an object with a "type" property like this.
+    if (msg.type === 'req:setText') {
+        const selections = figma.currentPage.selection['0'];
+        if (selections) {
+            yield figma.loadFontAsync(selections.fontName);
+            selections.characters = msg.reText;
+            selections.name = msg.textbox;
+        }
+        return;
+    }
+    if (msg.type === 'cancel') {
+        figma.closePlugin();
+    }
+    // Make sure to close the plugin when you're done. Otherwise the plugin will
+    // keep running, which shows the cancel button at the bottom of the screen.
+    figma.closePlugin();
+});
+figma.ui.postMessage(figma.currentPage.selection['0'].characters);
---
+++ code.ts
@@ -0,0 +1,49 @@
+// This plugin will open a modal to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
+
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (see documentation).
+
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
+
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+
+
+
+
+figma.ui.resize(300,400);
+
+
+figma.ui.onmessage = async msg => {
+  // One way of distinguishing between different types of messages sent from
+  // your HTML page is to use an object with a "type" property like this.
+
+
+  if(msg.type === 'req:setText'){
+
+    const selections  = figma.currentPage.selection['0'];
+    if(selections){
+      await figma.loadFontAsync(selections.fontName as FontName)
+      selections.characters = msg.reText;
+      selections.name = msg.textbox;
+    
+    
+    }
+    return
+  }
+
+  if(msg.type === 'cancel'){
+    figma.closePlugin();
+  }
+
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+  figma.closePlugin();
+};
+
+
+figma.ui.postMessage(figma.currentPage.selection['0'].characters);
